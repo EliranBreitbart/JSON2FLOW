75b5529a5ff59c08e483d8b178fe0e150bb541b0
EliranBreitbart <eliran9985@gmail.com>
2023-06-07 18:11:57 +0300

    fix
    one fix to the previous commit

--- src/pages/editor/EditorPage.jsx
+++ src/pages/editor/EditorPage.jsx
@@ -1,4 +1,4 @@
-import React, from "react";
+import React from "react";
 import Diagram2 from "../../components/diagram";
 import JsonView from "../../components/JsonView";
 import FileInfo from "../../components/FileInfo";
